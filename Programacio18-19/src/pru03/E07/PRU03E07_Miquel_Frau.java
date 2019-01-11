23ebfda7c8cf4da4bcbadf96cd3aeee1a2a0c9f7
miguel.frau <miguel.frau@PC-MFT.dynasoft.local>
2019-01-11 12:15:05 +0100

    V1.4

--- Programacio18-19/src/pru03/E07/PRU03E07_Miquel_Frau.java
+++ Programacio18-19/src/pru03/E07/PRU03E07_Miquel_Frau.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class PRU03E07_Miquel_Frau {
 	public static void main (String[] args) throws Exception{
+		
+		/* Se crea el Scanner y se crean las variables principales. */
+		
 		Scanner sc = new Scanner(System.in);
 
 		boolean repetir = true;
@@ -28,6 +31,9 @@ public class PRU03E07_Miquel_Frau {
 		int stock;
 		int seleccio_modificar;
 
+		/* Se crea una ArrayList de Articulos para almacenar todos los artículos que se van creando. Todo el código está envuelto en un bucle do while para controlar
+		 * cuando se ha de dejar de mostrar el menú. */
+		
 		ArrayList<Article> articles = new ArrayList<Article>();
 		do{
 			System.out.println("\nBenvingut a StorageManagement V1.4. Per favor, trii una de les opcions que es mostren a continuacio:");
@@ -39,10 +45,19 @@ public class PRU03E07_Miquel_Frau {
 			System.out.println("6. Sortida de mercaderia.");
 			System.out.println("7. Sortir.");
 
+			/* Pedimos al usuario que nos seleccione una de las opciones mostradas anteriormente y la introducimos a la variable seleccio, que se evalúa en un switch
+			 * para determinar qué debe hacer el programa. En caso de no introducir ninguno de los números mostrados en el menú, da un mensaje de error, diciendo que
+			 * no se ha introducido ninguna opción correcta.*/
+			
 			seleccio = sc.nextInt();
 
 			switch(seleccio) {
 			case 1:
+				
+				/*Sacamos una lista de todos los artículos que hay. Primeramente, comprobamos si hay algún artículo en la ArrayList. En caso de que no haya ninguno,
+				 * indicamos que no hay ninguna entrada para mostrar. En caso contrario, entramos en un bucle for que irá pasando por las distintas entradas de la
+				 * ArrayList e irá mostrándolas por pantalla.*/
+				
 				if (articles.size() == 0) {
 					System.out.println("No hi ha cap entrada per mostrar.");
 				}
@@ -52,7 +67,14 @@ public class PRU03E07_Miquel_Frau {
 				}
 				}
 				break;
-			case 2: repetir_alta_codi = true;
+				
+			case 2: 
+				
+				/* Introducimos un nuevo artículo. Lo primero que hacemos, es inicializar variables locales del caso, en su gran mayoría, booleans para ir controlando
+				 * las acciones que se realizan. Limpiamos el scanner y entramos en un bucle do while, para poder repetir el bucle en caso de que en algún punto haya
+				 * fallado algo.*/
+				
+			repetir_alta_codi = true;
 			repetir_alta_desc = true;
 			repetir_alta_stock = true;
 			flag_repetir_alta_desc = true;
@@ -62,6 +84,13 @@ public class PRU03E07_Miquel_Frau {
 			boolean repetir_alta_preuCompra = true;
 			sc.nextLine();
 			do {
+				
+				/*Inicializamos una variable llamada flag_mismo_articulo en false, para asegurarnos de que cada vez que se ejecuta el bucle, siempre empiece en false.
+				 * Esto se debe a que esa variable controla si se ha detectado que el código que se ha introducido ya pertenece a algún artículo. El sistema le pide
+				 * al usuario que introduzca el código del artículo que quiere añadir. Esto lo almacena en una variable y entra en un bucle for, que va revisando toda
+				 * la ArrayList mirando si ya hay algún artículo con el mismo código. Si no hay ningún artículo con el mismo código ya introducido, paramos el bucle de
+				 * repetir el código y enviamos un mensaje de confirmación */
+				
 				flag_mismo_articulo = false;
 				System.out.println("Per favor, introdugui el codi de l'article.\n");
 				codi_objeto = sc.nextLine();
@@ -76,6 +105,12 @@ public class PRU03E07_Miquel_Frau {
 				}
 			} while(repetir_alta_codi);
 			System.out.print("\nHa introduit: \""+codi_objeto+"\" com a codi satisfactòriament. ");
+			
+			/* Inicializamos flag_misma_desc en false para después comprobar si hay algún artículo con esa exacta descripción. En lugar de tacharla de error directamente,
+			 * se le pregunta al usuario si es correcto que haya dos descripciones iguales o no. Si dice que es correcto se termina el bucle de pedir la descripción y
+			 * si dice que no es correcto se repite que introduzca la descripción. Comprueba también si ha introducido una descripción diferente a las que ya hay y si es
+			 * así, para el bucle y envía un mensaje de confirmación.*/
+			
 			do {
 				flag_misma_desc = false;
 				System.out.print("Per favor, introdugui la descripció de l'article.\n");
@@ -105,7 +140,11 @@ public class PRU03E07_Miquel_Frau {
 				}
 			} while(repetir_alta_desc);
 			System.out.print("\nHa introduit: \""+desc_objeto+"\" com a descripció satisfactòriament. ");
-
+			
+			/* Se verifica en dos do while los precios de compra y de venta respectivamente que se introducen. En ambos, se revisa que no se introduzca
+			 * un valor negativo (en dicho caso salta un mensaje de error y repite la solicitud) hasta que introduce un número positivo. Al hacerlo, envía mensaje de
+			 * confirmación de valores.*/
+			
 			do {
 				System.out.print("Per favor, introdugui el preu de compra de l'article.\n");
 				preuCompra_objeto = sc.nextDouble();
@@ -129,7 +168,9 @@ public class PRU03E07_Miquel_Frau {
 					System.out.print("Has introduit un nombre negatiu i això no està acceptat. ");
 				}
 			} while (repetir_alta_preuVenda);
-
+			
+			/*Se solicita el stock del artículo en cuestión y se revisa que no sea un número negativo. Se confirma al finalizar */
+			
 			do {
 				System.out.print("Per favor, introdugui l'stock de l'article.\n");
 				stock = sc.nextInt();
@@ -140,6 +181,12 @@ public class PRU03E07_Miquel_Frau {
 					repetir_alta_stock = false;
 				}
 			} while(repetir_alta_stock);
+			System.out.print("\nHas introduit: \""+stock+"\" com a stock satisfactòriament. ");
+			
+			/*Una vez introducidos todos los datos necesarios, se añade a la ArrayList una nueva instancia de articulo. En el for, se navega por la ArrayList en busca
+			 * de una posición que tenga como código "LLIURE", que será la nueva instancia que se ha creado. Al haber encontrado ese código, se utilizan los setters
+			 * para añadir todos los datos introducidos anteriormente. */
+			
 			articles.add(new Article());
 			for (int i = 0; i<articles.size(); i++) {
 				if (articles.get(i).getCodi().equals("LLIURE")) {
@@ -152,11 +199,15 @@ public class PRU03E07_Miquel_Frau {
 			}
 			break;                        
 			case 3: 
+				
+				/*Se inicializan las variables locales y se limpia el Scanner. Se le pide al usuario que introduzca el código del artículo que quiere retirar. Se revisa
+				 * si hay una coincidencia y si la hay, se borra y se da mensaje de confirmación, si no, solicita al usuario si quiere buscar otro código.*/
+				
 				repetir_borrar = true;
-				flag_borrado = false;
 				flag_repetir_borrar = true;
 				sc.nextLine();
 				do {
+					flag_borrado = false;
 					System.out.println("Introdugui el codi de l'article que vol retirar.");
 					codi_objeto_retirar = sc.nextLine();
 					for (int i = 0; i<articles.size(); i++) {
@@ -189,6 +240,9 @@ public class PRU03E07_Miquel_Frau {
 				} while (repetir_borrar);
 				break;
 			case 4:
+				
+				/*Modificar un artículo ya existente. Se inicializan variables y se limpia Scanner. Se solicita a usuario que introduzca el código del artículo que quiere
+				 * modificar. Se verifica que ese artículo exista y se muestra un menú, en el que el usuario debe seleccionar la acción que quiere realizar.*/
 				repetir_modificar = true;
 				flag_modificado = false;
 				repetir_seleccio_modificar = true;
@@ -206,6 +260,8 @@ public class PRU03E07_Miquel_Frau {
 
 								switch(seleccio_modificar){
 								case 1: 
+									/*Cambiar el código del artículo. Se revisa que el código nuevo no sea el mismo que el de otro artículo, en cuyo caso da un mensaje de error. Si no
+									 * coincide con otro artículo, solicita al usuario si quiere hacer otra búsqueda.*/
 									repetir_modificar_nou = true;
 									sc.nextLine();
 									do {
@@ -227,7 +283,10 @@ public class PRU03E07_Miquel_Frau {
 									repetir_seleccio_modificar = false;
 									repetir_modificar = false;
 									break;
-								case 2: 
+								case 2:
+									
+									/*Cambiar la descripción actual por una nueva. */
+									
 									sc.nextLine();
 									System.out.println("Per favor, introdugui la nova descripció.");
 									String modificar_nou_desc = sc.nextLine();
@@ -236,6 +295,9 @@ public class PRU03E07_Miquel_Frau {
 									repetir_modificar = false;
 									break;
 								case 3: 
+									
+									/*Cambiar el precio de compra. Verifica que no se haya introducido un número negativo.*/
+									
 									System.out.println("Per favor, introdugui el nou preu de compra");
 									double modificar_nou_preuCompra = sc.nextDouble();
 									if (modificar_nou_preuCompra < 0){
@@ -247,7 +309,10 @@ public class PRU03E07_Miquel_Frau {
 										repetir_modificar = false;
 									}									
 									break;
-								case 4: 
+								case 4:
+									
+									/*Cambiar el precio de venta. Verifica que no se haya introducido un número negativo.*/
+									
 									System.out.println("Per favor, introdugui el nou preu de venda");
 									double modificar_nou_preuVenda = sc.nextDouble();
 									if (modificar_nou_preuVenda < 0) {
@@ -261,6 +326,7 @@ public class PRU03E07_Miquel_Frau {
 									
 									break;
 								case 5: 
+									/*Salir del menú de modificación*/
 									repetir_seleccio_modificar = false;
 									repetir_modificar = false;
 									break;
@@ -290,6 +356,11 @@ public class PRU03E07_Miquel_Frau {
 				} while (repetir_modificar);
 				break;
 			case 5: 
+				
+				/*Entrada de mercancía. Inicializa variables y limpia Scanner. Solicita al usuario el código del artículo al que le quiere añadir mercancía. Si encuentra el código,
+				 * pide al usuario cuanta mercancía ha entrado. Comprueba que no sea un número negativo y la introduce y da un mensaje afirmativo si todo ha salido bien. Si no se ha
+				 * encontrado el código del artículo al que le entra la mercancía, solicita al usuario si quiere introducir otro o no. */
+				
 				boolean flag_repetir_entrada = false;
 				boolean repetir_entrada = true;
 				boolean flag_entrada = false;
@@ -336,6 +407,12 @@ public class PRU03E07_Miquel_Frau {
 				} while (repetir_entrada);
 				break;
 			case 6:
+				
+				/* Salida de mercancía. Inicializa variables y limpia Scanner. Solicita al usuario el código del artículo al que le quiere sustraer mercancía. Si encuentra el código,
+				 * pide al usuario cuanta mercancía ha salido. Comprueba que no sea un número negativo y que la mercancia que se quiere retirar no sea mayor a la mercancía que hay en stock,
+				 * la introduce y da un mensaje afirmativo si todo ha salido bien. Si no se ha encontrado el código del artículo al que le entra la mercancía, solicita al 
+				 * usuario si quiere introducir otro o no. */
+				
 				boolean flag_repetir_salida = true;
 				boolean repetir_salida = true;
 				boolean flag_salida = false;
@@ -389,7 +466,11 @@ public class PRU03E07_Miquel_Frau {
 					}
 				} while (repetir_salida);
 				break;
-			case 7: repetir = false;
+			case 7: 
+				
+				/*Salir. Pone la variable que controla el bucle principal en false, para terminar el bucle y que acabe el programa.*/
+				
+				repetir = false;
 			break;
 			default: System.out.println("No has introduit cap opció correcta. Torna a introduir una opció.");
 			}

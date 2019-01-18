46895c6a402225fb34227c29243ec57bc52c3cb2
alumne-DAM <alumne-DAM@172.16.26.140>
2019-01-18 16:57:32 +0100

    V3.0

--- Programacio18-19/src/pru04/E02/PRU04E02_Miquel_Frau.java
+++ Programacio18-19/src/pru04/E02/PRU04E02_Miquel_Frau.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 
 public class PRU04E02_Miquel_Frau {
 	
+	//Mètode que circula per la linea i agafa el que es troba entre les dues posicions donades.
+	
 	public static String trobarParaula (String linea, int posicio_inicial, int posicio_final) {
 		String palabra = "";
 		
@@ -18,8 +20,15 @@ public class PRU04E02_Miquel_Frau {
 		}
 		return palabra;
 	}
-	public static String escriureParaula(String emp_no, String birth_date, String first_name, String last_name, String gender, String hire_date) {
+	
+	//Mètode que escriu a l'arxiu final la linea separada per ";" i amb un espai al final.
+	
+	
+	public static String escriureLinea(String emp_no, String birth_date, String first_name, String last_name, String gender, String hire_date) {
 		String linea_procesada = "";
+		String separador = ";";
+		String espacio = "\n";
+		linea_procesada = emp_no+separador+birth_date+separador+first_name+separador+last_name+separador+gender+separador+hire_date+espacio;
 		return linea_procesada;
 	}
 	public static void main (String[] args) {
@@ -29,23 +38,29 @@ public class PRU04E02_Miquel_Frau {
 			
 			String linea, emp_no, birth_date, first_name, last_name, gender, hire_date;
 			
+			//Revisa la lin
 		do {
 			linea = br.readLine();
 			if (linea != null) {
 				
 				linea += "";
 				
-				emp_no = trobarParaula(linea, 1, 7);
-				birth_date = trobarParaula(linea, 8, 17);
-				first_name = trobarParaula(linea, 18, 28);
-				last_name = trobarParaula(linea, 29, 42);
-				gender = trobarParaula(linea, 43, 49);
-				hire_date = trobarParaula(linea, 50, 59);
+				emp_no = trobarParaula(linea, 0, 5);
+				birth_date = trobarParaula(linea, 6, 15);
+				first_name = trobarParaula(linea, 16, 26);
+				last_name = trobarParaula(linea, 27, 40);
+				gender = trobarParaula(linea, 41, 47);
+				hire_date = trobarParaula(linea, 48, 57);
+				
+				bw.write(escriureLinea(emp_no, birth_date, first_name, last_name, gender, hire_date));
 				
 				
 			}
 			
 		}while (linea != null);
+		
+		br.close();
+		bw.close();
 		
 		} catch (IOException e) {
 			System.out.println("Error lectura/escritura");
